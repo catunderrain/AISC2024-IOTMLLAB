cfbce613072de4ed3593089bbb411599fa594f52
Thai Toan <catunderrainvn@gmail.com>
2024-08-03 22:39:18 +0700

    x

---
+++ CNN-A.MINH/main_0.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def sigmoid(x):
+    return np.e**x
+    return 1/(1+np.e**(-x))
+
+
+def dis_softmax():
+    xs = [i/10 for i in range(-160,160,1)]
+    ys = [sigmoid(x) for x in xs]
+    
+    plt.plot(xs,ys)
+    plt.scatter(5,sigmoid(5),c='r')
+    plt.show()
+    
+    return None
+
+
+if __name__ == '__main__':
+    dis_softmax()
+    pass
